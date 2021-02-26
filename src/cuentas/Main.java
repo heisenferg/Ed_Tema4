f56987687a14d82ae9a0caff329858fb5474f7f2
Fer <Fer@Fernando>
2021-02-26 17:59:25 +0100

    Operación commit. Comentando el resultado de la ejecución. La primera la realicé antes de ver el apartado del ejercicio, he realizado esta otra para insertar un comentario.

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("El saldo actual es"+ saldoActual );
 
         try {
-            cuenta1.retirar(23000);
+            cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
